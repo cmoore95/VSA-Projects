212d398603950c995b0afef4b2fb0d4623b92955
cmoore95 <cartermoore2002@gmail.com>
2017-06-19 11:26:55 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name: Carter Moore
+# Date: 6-19-17
 
 # proj01: A Simple Program
 # This program asks the user for his/her name and age.
@@ -7,4 +7,14 @@
 
 # If you complete extensions, describe your extensions here!
 
-
+name=raw_input('Enter your name: ')
+age = int(raw_input('How old are you? '))
+birth = raw_input('Have you had a birthday this year? ')
+yes = True
+no = False
+if (2017-age+100) > 100 and birth == no:
+    print name, 'will be 100 years old in', (2017-age+100)
+elif (2017-age+100) > 100:
+    print name, 'will be 100 years old in', (2017-age+99)
+else:
+    print name, 'was 100 years old in', (2017-age+100)
