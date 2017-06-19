50f58d2b5ad4bd6f57ae285a02bdaa0e457f4109
cmoore95 <cartermoore2002@gmail.com>
2017-06-19 15:10:20 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -10,11 +10,20 @@
 name=raw_input('Enter your name: ')
 age = int(raw_input('How old are you? '))
 birth = raw_input('Have you had a birthday this year? ')
-yes = True
-no = False
-if (2017-age+100) > 100 and birth == no:
-    print name, 'will be 100 years old in', (2017-age+100)
-elif (2017-age+100) > 100:
-    print name, 'will be 100 years old in', (2017-age+99)
+end_slice = name[1:]
+if age < 100 and birth == 'no' or 'No' or 'n' or 'N':
+    print name[0].upper()+end_slice, 'will be 100 in', (2017-age+99)
+elif age < 100 and birth == 'yes' or 'Yes' or 'y' or 'Y':
+    print name[0].upper()+end_slice, 'will be 100 in', (2017-age+100)
+elif age > 100 and birth == 'no' or 'No' or 'n' or 'N':
+    print name[0].upper()+end_slice, 'was 100 in', (2017-age+99)
 else:
-    print name, 'was 100 years old in', (2017-age+100)
+    print name[0].upper()+end_slice, 'was 100 in', (2017-age+100)
+if age > 17:
+    print 'You can watch movies rated R, PG-13, PG, and G'
+elif age > 12:
+    print 'You can watch movies rated PG-13, PG, and G'
+elif age > 5:
+    print 'You can watch movies rated PG, and G'
+else:
+    print 'You can watch movies rated G'
