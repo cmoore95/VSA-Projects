17fb3f0b0695308ee5290b3be377af55e9dc1ea8
cmoore95 <cartermoore2002@gmail.com>
2017-06-20 15:26:56 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -8,3 +8,17 @@ Asks the user for a string and prints out whether or not the string is a palindr
 
 """
 
+#shortcut
+
+
+phrase=raw_input('Enter a word or phrase. ')
+length=len(phrase)
+end=phrase[1:]
+
+phrase=phrase.lower()
+
+if phrase==phrase[::-1]:
+    print phrase[0].upper()+end,'is a palindrome'
+else:
+    print phrase[0].upper()+end,'is not a palindrome'
+
