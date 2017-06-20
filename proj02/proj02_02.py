66d5a457cb95271504ed4468d403ec44bfae45ec
cmoore95 <cartermoore2002@gmail.com>
2017-06-20 09:53:43 -0500

    Add files via upload

--- proj02/proj02_02.py
+++ proj02/proj02_02.py
@@ -11,12 +11,50 @@ previous two numbers in the sequence. The sequence looks like this:
 """
 
 
-total=int(raw_input('How many Fibonacci numbers would you like? '))
+total1 = int(raw_input('How many Fibonacci numbers would you like (FOR Loop)? '))
 
-number=1
-cur=1
-prev=1
-while total < (total + 1):
-    total = total + 1
-    cur=cur+prev
-    print 
+number1 = 0
+current1 = 1
+previous1 = 0
+old1 = previous1 + current1
+for number1 in range(total1):
+    old1 = current1
+    print current1
+    current1 = current1 + previous1
+    previous1 = old1
+
+
+total2=int(raw_input('How many Fibonacci numbers would you like (WHILE Loop)? '))
+
+number2=0
+current2=1
+previous2=0
+old2=previous2+current2
+while number2 < total2:
+    old2=current2
+    print current2
+    current2=current2+previous2
+    previous2=old2
+    number2=number2+1
+
+
+total3=int(raw_input('How many powers of 2 would you like? '))
+
+number3=1
+current3=1
+previous3=2
+while number3<(total3+ 1):
+    current3=current3*previous3
+    number3=number3+1
+    print current3
+
+
+total4=int(raw_input('What number do you want the divisors of? '))
+
+number4=1
+answer4=0
+while number4<(total4+1):
+    if total4%number4==0:
+        answer4=total4/number4
+        print answer4
+    number4=number4+1
